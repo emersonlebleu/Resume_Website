9cf05268ab0f50246a580d3bfb289ae22434411d
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-04-14 08:41:48 -0600

    Add Article model.

--- src/Resume_Website/articles/models.py
+++ src/Resume_Website/articles/models.py
@@ -1,3 +1,10 @@
+from cgitb import text
+from datetime import date
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.TextField(max_length=200)
+    text = models.TextField()
+    date = models.DateField()
+    topic = models.TextField(max_length=200)
