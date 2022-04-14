f76aef6d116d81d3375dfbcb5a8fc090b7d80752
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-04-14 15:17:29 -0600

    Base url incorperation of base template.

--- src/Resume_Website/Resume_Website/urls.py
+++ src/Resume_Website/Resume_Website/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import base_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('base/', base_view, name='base_view'),
 ]
