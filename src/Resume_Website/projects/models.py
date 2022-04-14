7fbc3482ce96b841a6c527b021109dc6f19c4ee9
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-04-14 08:45:41 -0600

    Add Project model.

--- src/Resume_Website/projects/models.py
+++ src/Resume_Website/projects/models.py
@@ -1,3 +1,10 @@
+from nturl2path import url2pathname
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.TextField()
+    url = models.URLField()
+    desciption = models.TextField()
+    authors = models.TextField(max_length=50)
