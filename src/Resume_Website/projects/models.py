bcb88e1765f2da3551ba360fc5ade5a1442b35ce
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-04-14 08:46:52 -0600

    Add constraint on Project.name

--- src/Resume_Website/projects/models.py
+++ src/Resume_Website/projects/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.TextField()
+    name = models.TextField(max_length=100)
     url = models.URLField()
     desciption = models.TextField()
     authors = models.TextField(max_length=50)
