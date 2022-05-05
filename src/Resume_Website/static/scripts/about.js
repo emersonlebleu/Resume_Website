bd5a41e0014fadbf23170010a44b6967f216dc4e
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-05-05 07:38:49 -0600

    Start js for welcome transition only on first load.

--- src/Resume_Website/static/scripts/about.js
+++ src/Resume_Website/static/scripts/about.js
@@ -1,4 +1,24 @@
 console.log("Working!");
 
+let firstLoad = sessionStorage.getItem("welcome")
+
+console.log(firstLoad)
+
+if (firstLoad == null) {
+    sessionStorage.setItem("welcome", "loaded"); 
+    let firstLoad = sessionStorage.getItem("welcome");
+    console.log("Inside First")
+    console.log(firstLoad)
+
+} else {
+    let w = document.getElementById("welcome-w").style
+    let el = document.getElementById("welcome-el").style
+    let come = document.getElementById("welcome-come").style
+    let ex_line = document.getElementById("welcome-ex-line").style
+    let ex_point = document.getElementById("welcome-ex-point").style
+
+    console.log("inside else")
+}
+
 
 
