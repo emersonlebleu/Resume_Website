907c21cba8ef1f4cdeab71a8d0561b214b154b7a
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-05-10 10:03:47 -0600

    Add sender address to email rec list.

--- src/Resume_Website/contact/forms.py
+++ src/Resume_Website/contact/forms.py
@@ -6,35 +6,32 @@ from django.core.mail import send_mail
 class ContactForm(forms.Form):
 
     name = forms.CharField(max_length=120)
-    email = forms.EmailField()
+    email = forms.EmailField(required=True)
     subject = forms.CharField(max_length=70)
     message = forms.CharField(widget=forms.Textarea)
 
     def get_info(self):
-        """
-        Method that returns formatted information
-        :return: subject, msg
-        """
         # Cleaned data
         cl_data = super().clean()
 
         name = cl_data.get('name').strip()
-        from_email = cl_data.get('email')
+        sender_email = cl_data.get('email')
         subject = cl_data.get('subject')
 
-        msg = f'{name} with email {from_email} said:'
+        msg = f'{name} said:'
         msg += f'\n"{subject}"\n\n'
         msg += cl_data.get('message')
+        msg += f'\n\n{name} email address: {sender_email}'
 
-        return subject, msg
+        return subject, msg, sender_email
 
     def send(self):
 
-        subject, msg = self.get_info()
+        subject, msg, sender_email = self.get_info()
 
         send_mail(
             subject=subject,
             message=msg,
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[settings.RECIPIENT_ADDRESS]
+            recipient_list=[settings.RECIPIENT_ADDRESS, sender_email]
         )
