f59dcb6c6b0927462a5577b6c81d724855a529c0
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-05-28 14:36:35 -0600

    Delete unused script.

--- src/Resume_Website/static/scripts/about.js
+++ src/Resume_Website/static/scripts/about.js
@@ -1,14 +1,4 @@
-console.log("Working!")
 
-let firstLoad = sessionStorage.getItem("loaded")
-
-if (firstLoad == null) {
-    sessionStorage.setItem("loaded", "yes") 
-
-} else {
-    document.getElementById("typed-heading").style.animation = "none";
-    document.getElementById("typed-heading").style.borderRight = "none";
-}
 
 
 
