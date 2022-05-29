933286e5d81283dbf4b940615d704910ceab0bbc
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-05-28 19:49:08 -0600

    Add links to scroll to sections in about.

--- src/Resume_Website/static/scripts/about.js
+++ src/Resume_Website/static/scripts/about.js
@@ -1,4 +1,26 @@
 
+function gToBackground() {
+    document.getElementById("background").scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+}
 
+function gToPersonalStatement() {
+    document.getElementById("personal-statement").scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+}
 
+function gToIntro() {
+    document.getElementById("introduction").scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' })
+}
 
+let bg_link = document.getElementById("bg-link")
+let ps_link = document.getElementById("ps-link")
+let intro_link = document.getElementById("intro-link")
+
+bg_link.addEventListener("click", function(){
+    gToBackground()
+})
+ps_link.addEventListener("click", function() {
+    gToPersonalStatement()
+})
+intro_link.addEventListener("click", function() {
+    gToIntro()
+})
