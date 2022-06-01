60852bad2da9d7993e59814aa4f8b36b387cf4ff
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-05-31 19:25:32 -0600

    Finish responsiveness for the inner nav.

--- src/Resume_Website/static/scripts/about.js
+++ src/Resume_Website/static/scripts/about.js
@@ -1,27 +1,48 @@
 // Scrolling functionality for the sidebar links
-function gToBackground() {
-    document.getElementById("background").scrollIntoView({ behavior: 'smooth', block: 'center'})
+function gToBackground(block) {
+    document.getElementById("background").scrollIntoView({ behavior: 'smooth', block: block})
 }
 
-function gToPersonalStatement() {
-    document.getElementById("personal-statement").scrollIntoView({ behavior: 'smooth', block: 'center'})
+function gToPersonalStatement(block) {
+    document.getElementById("personal-statement").scrollIntoView({ behavior: 'smooth', block: block})
 }
 
-function gToIntro() {
-    document.getElementById("introduction").scrollIntoView({ behavior: 'smooth', block: 'center'})
+function gToIntro(block) {
+    document.getElementById("introduction").scrollIntoView({ behavior: 'smooth', block: block})
 }
 
 let bg_link = document.getElementById("bg-link")
 let ps_link = document.getElementById("ps-link")
 let intro_link = document.getElementById("intro-link")
 
+let start = 'start'
+let center = 'center'
+
 bg_link.addEventListener("click", function(){
-    gToBackground()
+    let width = window.innerWidth
+    if (width <= 770){
+        gToBackground(start)    
+    } 
+    else {
+        gToBackground(center) 
+    }
 })
 ps_link.addEventListener("click", function() {
-    gToPersonalStatement()
+    let width = window.innerWidth
+    if (width <= 770){
+        gToPersonalStatement(start)    
+    } 
+    else {
+        gToPersonalStatement(center) 
+    }
 })
 intro_link.addEventListener("click", function() {
-    gToIntro()
+    let width = window.innerWidth
+    if (width <= 770){
+        gToIntro(start)    
+    } 
+    else {
+        gToIntro(center) 
+    }
 })
 
