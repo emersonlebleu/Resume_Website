9e8e7f496b98a923fa5622650665bdaaad2659fc
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-07-30 10:33:07 -0600

    Add confirmation field.

--- src/Resume_Website/contact/forms.py
+++ src/Resume_Website/contact/forms.py
@@ -9,6 +9,7 @@ class ContactForm(forms.Form):
     email = forms.EmailField(required=True)
     subject = forms.CharField(max_length=70, required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
+    confirm = forms.ChoiceField(choices=[("robot", "Robot"), ("non-human", "Non-Human"), ("script", "Script"), ("human", "Person")], required=True, widget=forms.Select, label="What are you?")
 
     def get_info(self):
         # Cleaned data
@@ -17,19 +18,20 @@ class ContactForm(forms.Form):
         name = cl_data.get('name').strip()
         sender_email = cl_data.get('email')
         subject = cl_data.get('subject')
+        is_person = cl_data.get('confirm')
 
         msg = f'"{name}" @email address: {sender_email} sent:\n\n'
         msg += cl_data.get('message')
 
-        return subject, msg, sender_email
+        return subject, msg, sender_email, is_person
+
 
     def send(self):
-
-        subject, msg, sender_email = self.get_info()
-
-        send_mail(
-            subject=subject,
-            message=msg,
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[settings.RECIPIENT_ADDRESS, sender_email]
-        )
+        subject, msg, sender_email, is_person = self.get_info()
+        if is_person == 'human':
+            send_mail(
+                subject=subject,
+                message=msg,
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[settings.RECIPIENT_ADDRESS, sender_email]
+            )
