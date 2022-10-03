07a1bb0a9aa33650ae5b09573e4851d213a7c2a5
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-10-03 14:57:03 -0600

    Change to test
    Changed debug to true for changes.

--- src/Resume_Website/Resume_Website/settings.py
+++ src/Resume_Website/Resume_Website/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # change to FALSE before pull live
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
